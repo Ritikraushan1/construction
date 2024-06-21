7bfba2c199702e1744c2ffdff51453515596a502
Ritikraushan1 <rkr0978@gmail.com>
2024-06-21 14:25:15 +0530

    hero section without header

---
+++ app/components/Hero.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import { GoArrowUpRight } from "react-icons/go";
+
+function Hero() {
+    return (
+        <div className=' bg-hero h-screen'>
+            <div className="absolute pt-[35%] text-black">
+                <p className=' text-5xl ml-20 font-semibold text-white leading-16'>APARTMENT <span className=' ml-20 text-yellow-400'>RENOVATIONS</span><br />IN MANCHESTER AND LEVERPOOL</p>
+            </div>
+            <div className="grid grid-rows-5 h-full">
+                {/* 1st column */}
+                <div className="grid grid-cols-7">
+                    <div className=" border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300  border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                </div>
+                {/* 2nd column */}
+                <div className="grid grid-cols-7">
+                    <div className=" border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30 flex items-center justify-center">
+                        <h1 className="text-white text-xs text-center font-bold opacity-60">Repair<br /> and construction<br /> company</h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30 bg-black">
+                        <div className=' px-5 py-5 flex flex-col items-start justify-between h-full'>
+                            <p className=' text-xs text-yellow-300'>/01</p>
+                            <p className=' text-xs opacity-50'>Feel your idea and<br /> make a design product.</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                </div>
+                {/* 3rd column */}
+                <div className="grid grid-cols-7">
+                    <div className=" border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30 bg-black">
+                        <div className=' px-5 py-5 flex flex-col items-start justify-between h-full'>
+                            <p className=' text-xs text-yellow-300'>/02</p>
+                            <p className=' text-xs opacity-50'>We make repairs with<br /> a 5 year warranty and <br />post warranty service.</p>
+                        </div>
+                    </div>
+                </div>
+                {/* 4th column */}
+                <div className="grid grid-cols-7">
+                    <div className=" border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30 bg-black">
+                        <div className=' px-5 py-5 flex flex-col items-start justify-between h-full'>
+                            <p className=' text-xs text-yellow-300'>/03</p>
+                            <p className=' text-xs opacity-50'>We will select and<br /> purchase materials.</p>
+                        </div>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                </div>
+                {/* 5th column */}
+                <div className="grid grid-cols-7">
+                    <div className=" border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30 bg-black">
+                        <div className='text-yellow-300 mr-2 flex flex-col py-5 px-3 items-end'><GoArrowUpRight /></div>
+                        <h1 className="text-white text-xs font-bold items-center flex flex-col py-5 h-full">
+                            <p>Calculate Repair Cost</p>
+                        </h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                    <div className="border-[1px] border-yellow-300 border-opacity-30">
+                        <h1 className="text-white text-2xl font-bold"></h1>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Hero
--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
 import About from "./components/About";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className=" bg-black">
+      <Hero />
       <About />
       <Services />
       <Projects />
