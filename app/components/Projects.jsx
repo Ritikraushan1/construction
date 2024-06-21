208276115258672d8924fd4a8b9ea2e5a13f3f3c
Ritikraushan1 <rkr0978@gmail.com>
2024-06-21 10:56:15 +0530

    services and projects

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -4,19 +4,19 @@ import React from 'react'
 export default function Projects() {
     return (
         <div className=' py-12'>
-            <div className=" flex text-white">
+            <div className=" flex text-white flex-col-1 lg:flex-col-3">
                 <div className=" bg-custom3 w-[33%] h-[80vh]">
-                    <div className=" ml-10 text-yellow-300 text-lg font-serif mt-[115%] uppercase">
+                    <div className=" absolute ml-10 text-white mt-[38%] text-lg font-serif uppercase">
                         Arch Building<br /> Projects
                     </div>
                 </div>
                 <div className=" bg-custom1 w-[33%] h-[80vh]">
-                    <div className=" ml-10 text-yellow-300 text-lg font-serif mt-[115%] uppercase">
+                    <div className=" absolute ml-10 text-white mt-[38%] text-lg font-serif uppercase">
                         Furniture Design<br /> Projects
                     </div>
                 </div>
                 <div className=" bg-custom2 w-[33%] h-[80vh]">
-                    <div className=" ml-10 text-yellow-300 text-lg font-serif mt-[115%] uppercase">
+                    <div className=" absolute ml-10 text-white mt-[38%] text-lg font-serif uppercase">
                         Our Interior<br /> Projects
                     </div>
                 </div>
