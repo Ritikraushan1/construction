44f326856f9c7e14b5415db00ace77fcc868dd75
Ritikraushan1 <rkr0978@gmail.com>
2024-06-22 10:29:47 +0530

    projects modified

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -4,29 +4,29 @@ import React from 'react'
 export default function Projects() {
     return (
         <div className='w-screen h-screen'>
-            <div className=" flex text-white flex-col-1 lg:flex-col-3">
-                <div className=" bg-custom3 w-[33%] h-[100vh]">
-                    <div className="bg-black bg-opacity-40 h-full">
-
-                        <div className=" absolute ml-10 text-white mt-[48%] text-lg font-serif uppercase">
-                            Arch Building<br /> Projects
-                        </div>
+            <div className=" grid grid-cols-1 lg:grid-cols-3 text-white grid-flow-col-dense">
+                <div className="relative">
+                    <div className="">
+                        <img src="/images/img4.jpg" alt="project1" className=' w-full h-screen opacity-60 object-cover ' />
+                    </div>
+                    <div className="absolute bottom-10 left-10">
+                        <p className=' text-xl font-bold uppercase font-rufina'>Arch Building <br /> Projects</p>
                     </div>
                 </div>
-                <div className=" bg-custom1 w-[33%] h-[100vh] bg-blend-overlay">
-                    <div className="bg-black bg-opacity-40 h-full">
-
-                        <div className=" absolute ml-10 text-white mt-[48%] text-lg font-serif uppercase">
-                            Furniture Design<br /> Projects
-                        </div>
+                <div className="relative h-full">
+                    <div className="">
+                        <img src="/images/img2.jpg" alt="project1" className='w-full h-screen opacity-60 object-cover' />
+                    </div>
+                    <div className="absolute bottom-10 left-10">
+                        <p className=' text-xl font-bold uppercase font-rufina'>Furniture Design <br /> Projects</p>
                     </div>
                 </div>
-                <div className=" bg-custom2 w-[33%] h-[100vh] bg-blend-overlay">
-                    <div className="bg-black bg-opacity-40 h-full">
-
-                        <div className=" absolute ml-10 text-white mt-[48%] text-lg font-serif uppercase">
-                            Our Interior<br /> Projects
-                        </div>
+                <div className="relative h-full">
+                    <div className="">
+                        <img src="/images/img3.jpg" alt="project1" className='w-full h-screen opacity-60 object-cover' />
+                    </div>
+                    <div className="absolute bottom-10 left-10">
+                        <p className=' text-xl font-bold uppercase font-rufina'>Our Interior <br /> Projects</p>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        rufina: ["Rufina", "serif"],
+        dmsans: ["DM Sans", "sans-serif"],
+      },
     },
   },
   plugins: [],
