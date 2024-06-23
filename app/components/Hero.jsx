81b51b2b974aaa25b7d0a5464d9185acd1f3b8d3
Ritikraushan1 <rkr0978@gmail.com>
2024-06-23 19:30:08 +0530

    hero section updated and responsive

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -11,10 +11,10 @@ function Hero() {
     return (
         <div className=' relative bg-hero h-screen w-screen'>
             <div className=' w-screen h-screen bg-black bg-opacity-60 z-0'>
-                <div className="absolute pt-[30%] text-black z-40">
-                    <p className=' text-5xl ml-20 font-semibold text-white leading-16'>APARTMENT <span className=' ml-20 text-yellow-400'>RENOVATIONS</span><br />IN MANCHESTER AND LEVERPOOL</p>
+                <div className="absolute pt-[80%] lg:pt-[30%] text-black z-40">
+                    <p className='text-2xl lg:text-5xl ml-5 lg:ml-20 font-semibold text-white leading-16'>APARTMENT <span className=' ml-5 lg:ml-20 text-yellow-400'>RENOVATIONS</span><br />IN MANCHESTER AND LEVERPOOL</p>
                 </div>
-                <div className="grid grid-rows-5 h-full z-50">
+                <div className=" hidden lg:grid grid-rows-5 h-full z-50">
                     {/* 1st column */}
                     <div className="grid grid-cols-7">
                         <div className=" border-[1px] border-white border-opacity-5">
@@ -45,7 +45,7 @@ function Hero() {
                             <h1 className="text-white text-2xl font-bold"></h1>
                         </div>
                         <div className="border-[1px] border-white border-opacity-5 flex items-center justify-center">
-                            <h1 className="text-white text-xs text-center font-bold opacity-60">Repair<br /> and construction<br /> company</h1>
+                            <h1 className="text-white text-xs text-center opacity-50">Repair<br /> and construction<br /> company</h1>
                         </div>
                         <div className="border-[1px] border-white border-opacity-5">
                             <h1 className="text-white text-2xl font-bold"></h1>
@@ -128,7 +128,7 @@ function Hero() {
                         </div>
                         <div className="border-[1px] border-white border-opacity-5 hover:bg-[#000000] hover:bg-opacity-100">
                             <div className='text-yellow-300 mr-2 flex flex-col py-5 px-3 items-end '><GoArrowUpRight /></div>
-                            <h1 className="text-white text-xs font-bold items-center flex flex-col py-5 h-full">
+                            <h1 className="text-white text-xs opacity-50 items-center flex flex-col py-5 h-full">
                                 <p>Calculate Repair Cost</p>
                             </h1>
                         </div>
